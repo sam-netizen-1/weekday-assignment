ef89f1833cd18f9f8ed97e5ebe0c752ffb7a9788
samkit-coditas <samkit.kothari@coditas.com>
2024-05-03 17:29:58 +0530

    Added React Select instead of Mui for filters

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,60 +1,70 @@
+import React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import MenuItem from "@mui/material/MenuItem";
 import styles from "./Filters.module.scss";
-import { Select } from "@mui/material";
+import Select from "react-select";
+import { MenuItem } from "@mui/material";
 
 interface FiltersProps {
   onFilterChange: any;
-  // onFilterChange: (filterName: string, value: string | number) => void;
 }
 
 const roles = ["frontend", "ios", "android", "tech lead", "backend"];
-const experiences = ["<1 year", "1-3 years", "3-5 years", "5+ years"];
+const experiences = ["1 ", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 const locations = ["remote", "delhi ncr", "mumbai", "chennai", "bangalore"];
+const basePayRanges = ["<10k", "10k-30k", "30k-50k", "50k+"];
 
 const Filters = ({ onFilterChange }: FiltersProps) => {
+  const handleSelectChange = (selectedOption: any, filterName: string) => {
+    const selectedValues = selectedOption ? selectedOption.value : "";
+    onFilterChange(filterName, selectedValues);
+  };
+
   return (
     <Box className={styles.filtersBar}>
-      <TextField
-        select
-        label="Role"
-        defaultValue=""
-        onChange={(event) => onFilterChange("role", event.target.value)}
-        className={styles.filterItem}
-      >
-        {roles.map((option) => (
-          <MenuItem key={option} value={option}>
-            {option}
-          </MenuItem>
-        ))}
-      </TextField>
       <Select
-        multiple
-        label="Experience"
-        defaultValue={[]}
-        onChange={(event) => onFilterChange("experience", event.target.value)}
+        options={experiences.map((exp) => ({ value: exp, label: exp }))}
+        placeholder="Min Experience"
+        onChange={(selectedOption) =>
+          handleSelectChange(selectedOption, "experience")
+        }
         className={styles.filterItem}
-      >
-        {experiences.map((option) => (
-          <MenuItem key={option} value={option}>
-            {option}
-          </MenuItem>
-        ))}
-      </Select>
+        isClearable
+      />
+      <Select
+        options={locations.map((loc) => ({ value: loc, label: loc }))}
+        placeholder="Location"
+        onChange={(selectedOption) =>
+          handleSelectChange(selectedOption, "location")
+        }
+        className={styles.filterItem}
+        isMulti
+        isClearable
+      />
+      <Select
+        options={roles.map((role) => ({ value: role, label: role }))}
+        placeholder="Role"
+        onChange={(selectedOption) =>
+          handleSelectChange(selectedOption, "role")
+        }
+        className={styles.filterItem}
+        isMulti
+        isClearable
+      />
+      <Select
+        options={basePayRanges.map((pay) => ({ value: pay, label: pay }))}
+        placeholder="Min Base Pay"
+        onChange={(selectedOption) =>
+          handleSelectChange(selectedOption, "basePay")
+        }
+        className={styles.filterItem}
+        isClearable
+      />
       <TextField
-        select
-        label="Locations"
-        defaultValue=""
-        onChange={(event) => onFilterChange("remote", event.target.value)}
+        label="Company Name"
+        onChange={(event) => onFilterChange("companyName", event.target.value)}
         className={styles.filterItem}
-      >
-        {locations.map((option) => (
-          <MenuItem key={option} value={option}>
-            {option}
-          </MenuItem>
-        ))}
-      </TextField>
+      />
     </Box>
   );
 };
